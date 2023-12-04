62d98f2478e6def95b8e5a15ae0f7291af4a08b1
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-12-04 14:40:02 -0800

    Adding implementation for apply_on_update

--- manual_tests/behaviors/apply_on_update.py
+++ manual_tests/behaviors/apply_on_update.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.functions import now
+from manual_tests.lib import gh
+from manual_tests.lib import gha
+from manual_tests.lib import slice
+from manual_tests.lib import tacos_demo
+from manual_tests.lib import tf
+
+TEST_NAME = __name__
+
+Branch = int
+
 
 def test() -> None:
-    # TODO: anything
-    pass
+    tacos_demo.clone()
+    since = now()
+
+    tacos_demo_pr = tacos_demo.new_pr(TEST_NAME, slice.random())
+    gh.approve_pr(tacos_demo_pr.url)
+
+    gh.assert_pr_is_approved(tacos_demo_pr.url)
+
+    gha.assert_eventual_success("terraform_lock", since)
+
+    since = now()
+
+    assert not tf.plan_clean()
+
+    gh.add_label(tacos_demo_pr.url, ":taco::apply")
+
+    gha.assert_eventual_success("terraform_apply", since)
+
+    assert tf.plan_clean()
+
+    gh.close_pr(tacos_demo_pr.branch)
