ac2b0bcb4a600ad6eb80f75bd8cc4c95be4bf7c2
Ellison Marks <ellison.marks@sentry.io>
2023-12-05 14:13:12 -0800

    Adds tests for the :tacos::unlock label.

--- manual_tests/behaviors/force_unlock_on_dirty.py
+++ manual_tests/behaviors/force_unlock_on_dirty.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.functions import now
+from manual_tests.lib import gh
+from manual_tests.lib import gha
+from manual_tests.lib import slice
+from manual_tests.lib import tacos_demo
+
+TEST_NAME = __name__
+
 
 def test() -> None:
-    # TODO: anything
-    pass
+    tacos_demo.clone()
+
+    since = now()
+    tacos_demo_pr = tacos_demo.new_pr(TEST_NAME, slice.random())
+    try:
+        gha.assert_eventual_success("terraform_lock", since)
+        gh.add_label(tacos_demo_pr.url, ":taco::apply")
+        gha.assert_eventual_success("terraform_apply", since)
+        gh.add_label(tacos_demo_pr.url, ":taco::unlock")
+        gha.assert_eventual_success("terraform_unlock", since)
+        gh.assert_matching_comment(
+            "WARNING: Unlocked while applied but not merged!", since
+        )
+    finally:
+        gh.close_pr(tacos_demo_pr.branch)
--- manual_tests/behaviors/force_unlock_on_happy_path.py
+++ manual_tests/behaviors/force_unlock_on_happy_path.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.functions import now
+from manual_tests.lib import gh
+from manual_tests.lib import gha
+from manual_tests.lib import slice
+from manual_tests.lib import tacos_demo
+
+TEST_NAME = __name__
+
 
 def test() -> None:
-    # TODO: anything
-    pass
+    tacos_demo.clone()
+
+    since = now()
+    tacos_demo_pr = tacos_demo.new_pr(TEST_NAME, slice.random())
+    try:
+        gha.assert_eventual_success("terraform_lock", since)
+        gh.add_label(tacos_demo_pr.url, ":taco::unlock")
+        # nothing has been applied, this should succeed.
+        gha.assert_eventual_success("terraform_unlock", since)
+    finally:
+        gh.close_pr(tacos_demo_pr.branch)
