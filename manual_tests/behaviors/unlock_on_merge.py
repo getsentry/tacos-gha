4b0368df008872c78cd673d427f5d21e155d4268
Ellison Marks <ellison.marks@sentry.io>
2023-12-07 14:47:39 -0800

    Adds `since` parameters for the `assert_eventual_success` calls.

--- manual_tests/behaviors/unlock_on_merge.py
+++ manual_tests/behaviors/unlock_on_merge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.functions import now
 from manual_tests.lib import gha
 from manual_tests.lib import slice
 from manual_tests.lib import tacos_demo
@@ -11,16 +12,19 @@ TEST_NAME = __name__
 def test() -> None:
     slices = slice.random()
 
+    since = now()
     tacos_demo.clone()
     tacos_demo_pr = tacos_demo.PR.for_test(TEST_NAME, slices)
     try:
-        gha.assert_eventual_success(tacos_demo_pr, "terraform_lock")
+        gha.assert_eventual_success(tacos_demo_pr, "terraform_lock", since)
+        since = now()
         tacos_demo_pr.approve()
         assert tacos_demo_pr.approved()
         tacos_demo_pr.add_label(":taco::apply")
-        gha.assert_eventual_success(tacos_demo_pr, "terraform_apply")
+        gha.assert_eventual_success(tacos_demo_pr, "terraform_apply", since)
+        since = now()
         tacos_demo_pr.merge_pr()
     except Exception:  # If we manage to merge, we don't need to close.
         tacos_demo_pr.close()
         raise
-    gha.assert_eventual_success(tacos_demo_pr, "terraform_unlock")
+    gha.assert_eventual_success(tacos_demo_pr, "terraform_unlock", since)
