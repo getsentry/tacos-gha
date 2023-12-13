9b7ab4f0f49d5ed7ab834cd5f80c29ce736d6381
Buck Evan <buck.evan@sentry.io>
2023-12-13 15:44:24 -0600

    wip: drift remediation

--- manual_tests/scenarios/drift_remediation.py
+++ manual_tests/scenarios/drift_remediation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.sh import sh
+from manual_tests.lib import tacos_demo
 
-def test() -> None:
-    # TODO: anything
-    pass
+
+def test(pr: tacos_demo.PR) -> None:
+    sh.banner("somebody changes infrastructure out of band")
+    sh.banner("pretend an hour passed: trigger the drift-scan job")
+    sh.banner("check out automatically-created PR")
+    sh.banner("edit and/or apply till plan is clean")
+    sh.banner("request unlock")
