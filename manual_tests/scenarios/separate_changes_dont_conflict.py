076481d29b65d8f5babe296ef481bb7e5ecd216c
Ellison Marks <ellison.marks@sentry.io>
2023-12-13 10:54:10 -0800

    Adds test for acquiring disjoint locks.

--- manual_tests/scenarios/separate_changes_dont_conflict.py
+++ manual_tests/scenarios/separate_changes_dont_conflict.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+import pytest
 
+from lib.functions import now
+from lib.sh import sh
+from manual_tests.lib import tacos_demo
+from manual_tests.lib.slice import Slice
+from manual_tests.lib.slice import Slices
+
+TEST_NAME = __name__
+
+
+@pytest.mark.xfail(reason="Locking not yet implemented")
 def test() -> None:
-    # TODO: anything
-    pass
+    slice1 = Slices([Slice(0)])
+    slice2 = Slices([Slice(1)])
+
+    since = now()
+    tacos_demo.clone()
+    sh.banner(f"User 1 opens a PR for slice: {slice1}")
+    sh.banner(f"User 2 opens a PR for slice: {slice2}")
+    with (
+        tacos_demo.PR.opened_for_test(TEST_NAME, slice1) as pr1,
+        tacos_demo.PR.opened_for_test(TEST_NAME, slice2) as pr2,
+    ):
+        sh.banner("User 1 acquires the lock")
+        assert pr1.check("terraform_lock").wait(since).success
+
+        sh.banner("User 2 acquires the lock")
+        assert pr2.check("terraform_lock").wait(since).success
