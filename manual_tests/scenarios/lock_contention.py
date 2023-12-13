d8342d67dea25a7e45d3051c97e0e938604d61cf
Ellison Marks <ellison.marks@sentry.io>
2023-12-13 01:01:36 -0800

    Adds test for lock contention scenario.

--- manual_tests/scenarios/lock_contention.py
+++ manual_tests/scenarios/lock_contention.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+import pytest
 
+from lib.functions import now
+from manual_tests.lib import slice
+from manual_tests.lib import tacos_demo
+
+TEST_NAME = __name__
+
+
+@pytest.mark.xfail(reason="Locking not yet implemented")
 def test() -> None:
-    # TODO: anything
-    pass
+    slices = slice.random()
+
+    since = now()
+    tacos_demo.clone()
+    pr1 = tacos_demo.PR.for_test(TEST_NAME, slices)
+    pr1_open = True
+    pr2 = tacos_demo.PR.for_test(TEST_NAME, slices)
+    try:
+        assert pr1.check("terraform_lock").wait(since).success
+        assert "TODO: add comment about lock failure here" in pr2.comments(
+            since=since
+        )
+        pr1.close()
+        pr1_open = False
+        pr2.add_label(":taco::acquire-lock")
+        since = now()
+        assert pr2.check("terraform_lock").wait(since).success
+    finally:
+        if pr1_open:
+            pr1.close()
+        pr2.close()
