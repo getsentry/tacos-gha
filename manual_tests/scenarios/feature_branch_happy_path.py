f38f6da2c770470a6ab7685fc1331ea8f886b337
Buck Evan <buck.evan@sentry.io>
2023-12-12 18:25:02 -0600

    done: happy path

--- manual_tests/scenarios/feature_branch_happy_path.py
+++ manual_tests/scenarios/feature_branch_happy_path.py
@@ -7,19 +7,13 @@ from lib.functions import now
 from lib.sh import sh
 from manual_tests.lib import tacos_demo
 from manual_tests.lib import tf
+from manual_tests.lib.gh import gh
+from manual_tests.lib.slice import Slices
+from manual_tests.lib.xfail import XFailed
+from manual_tests.lib.xfail import XFails
 
 
-@pytest.mark.xfail(reason="Need another user's approval")
-def test(pr: tacos_demo.PR) -> None:
-    xfail: list[tuple[bool, object]] = []
-
-    # look at your plan
-    plan = pr.get_plan()
-    assert plan
-
-    pr.approve()
-    assert pr.approved()
-
+def apply(pr: gh.PR, xfail: XFails) -> None:
     # the taco-apply label causes the plan to become clean:
     assert not tf.plan_clean()
     since = now()
@@ -29,11 +23,10 @@ def test(pr: tacos_demo.PR) -> None:
     try:
         assert tf.plan_clean()
     except AssertionError:
-        xfail.append((False, "plan not clean"))
+        xfail.append(("assert tf.plan_clean()", "plan not clean"))
 
-    pr.merge()
 
-    # show it really did merge
+def assert_merged(xfail: XFails) -> None:
     sh.run(("git", "remote", "update"))
     merged_sha = sh.stdout(("git", "log", "-1", "--format=%H"))
     lines = sh.lines(("git", "branch", "--remotes", "--contains", merged_sha))
@@ -42,8 +35,37 @@ def test(pr: tacos_demo.PR) -> None:
     except AssertionError:
         xfail.append(("assert lines", lines))
 
-    # but at the same time, terraform-plan is clean
+
+@pytest.mark.xfail(reason="apply not yet implemented", raises=XFailed)
+def test(pr: tacos_demo.PR, test_name: str) -> None:
+    xfail: XFails = []
+
+    sh.banner("look at your plan")
+    plan = pr.get_plan()
+    assert plan
+
+    sh.banner("apply")
+    pr.approve()  # needs at least one approval
+    assert pr.approved()
+    apply(pr, xfail)
+
+    sh.banner("edit, more")
+    tacos_demo.commit_changes_to(Slices.random(), test_name, pr.branch)
+
+    sh.banner("apply, again")
+    apply(pr, xfail)
+
+    sh.banner("merge")
+    pr.merge()
+
+    sh.banner("show it really did merge")
+    assert_merged(xfail)
+
+    sh.banner("show terraform-plan really is clean")
     try:
         assert tf.plan_clean()
     except AssertionError:
         xfail.append(("assert tf.plan_clean()", "plan not clean"))
+
+    if xfail:
+        raise XFailed(xfail)
