aff6ec7600b8f1569d9c20f97b8ba1d7e2fd990f
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-01-03 15:44:49 -0600

    Cleaning up force_unlock_on_happy_path test, closer to passing

--- manual_tests/behaviors/force_unlock_on_happy_path.py
+++ manual_tests/behaviors/force_unlock_on_happy_path.py
@@ -4,14 +4,23 @@ from __future__ import annotations
 import pytest
 
 from manual_tests.lib import tacos_demo
+from manual_tests.lib.gh import gh
 from manual_tests.lib.xfail import XFailed
 
 TEST_NAME = __name__
 
 
 @pytest.mark.xfail(raises=XFailed)
-def test(pr: tacos_demo.PR) -> None:
-    assert pr.check("terraform_lock").wait().success
+def test(pr: tacos_demo.PR, git_clone: gh.repo.Local) -> None:
+    slices = pr.slices
+    for s in slices:
+        assert (
+            pr.check(
+                f"terraform_lock ({(slices.workdir / s).relative_to(git_clone.path)})"
+            )
+            .wait()
+            .success
+        )
 
     since = pr.add_label(":taco::unlock")
     assert pr.check("terraform_unlock").wait(since).success
