927426c8317a88ac7ac3b33764197776db6d638b
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-01-08 16:08:39 -0800

    Fixing tf-lock-release and updating force unlock test

--- manual_tests/behaviors/force_unlock_on_happy_path.py
+++ manual_tests/behaviors/force_unlock_on_happy_path.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+import re
+
 import pytest
 
 from manual_tests.lib import tacos_demo
@@ -9,17 +11,50 @@ from manual_tests.lib.xfail import XFailed
 TEST_NAME = __name__
 
 
+def assert_slices_unlocked_in_comment(
+    slices: tacos_demo.Slices, comment: str, git_clone: gh.repo.Local
+) -> None:
+    for s in slices:
+        slice_path = (slices.workdir / s).relative_to(git_clone.path)
+        already_unlocked = (
+            re.search(f"success: already unlocked: {slice_path}", comment)
+            is not None
+        )
+        lock_released = (
+            re.search(
+                f"success: released lock \(from .+\): {slice_path}", comment
+            )
+            is not None
+        )
+        assert (
+            already_unlocked or lock_released
+        ), f"slice {slice_path} not unlocked"
+
+
 @pytest.mark.xfail(raises=XFailed)
-def test(pr: tacos_demo.PR) -> None:
-    # TODO: use slice name
-    assert pr.check("Terraform Lock").wait().success
+def test(pr: tacos_demo.PR, git_clone: gh.repo.Local) -> None:
+    slices = pr.slices
+    for slice in slices:
+        assert (
+            pr.check("Terraform Lock", f"tacos-gha / main ({slice})")
+            .wait()
+            .success
+        )
 
     since = pr.add_label(":taco::unlock")
-    assert pr.check("Terraform Unlock", "terraform_unlock").wait(since).success
+    assert pr.check("terraform_unlock").wait(since).success
+    assert pr.check("summarize").wait(since).success
+    comments = pr.comments(since=since)
 
-    try:
-        assert "INFO: Main branch clean, unlock successful." in pr.comments(
-            since=since
-        )
-    except AssertionError:
-        raise XFailed("still need to post message from unlock")
+    # Find the terraform lock state comment
+    lock_state_comment = None
+    for comment in comments:
+        if "Terraform lock state:" in comment:
+            lock_state_comment = comment
+            break
+
+    assert (
+        lock_state_comment is not None
+    ), "Terraform lock state comment not found"
+
+    assert_slices_unlocked_in_comment(slices, comment, git_clone)
