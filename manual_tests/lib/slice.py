90ee2f44f8f325052964d6aed0bad9d17d812a03
Buck Evan <buck.evan@sentry.io>
2024-01-15 17:36:20 -0600

    fix: relpaths, systemically

--- manual_tests/lib/slice.py
+++ manual_tests/lib/slice.py
@@ -5,7 +5,6 @@ from random import Random
 from typing import TYPE_CHECKING
 
 from lib.constants import EMPTY_PATH
-from lib.constants import REPO_TOP
 from lib.functions import now
 from lib.sh import sh
 from lib.types import Generator
@@ -23,7 +22,7 @@ class Slice(Path):
 
     def is_locked(self, workdir: Path) -> bool:
         with sh.cd(workdir / self):
-            j = sh.json(("sudo-sac", REPO_TOP / "lib/tf-lock/tf-lock-info"))
+            j = sh.json(("sudo-sac", "tf-lock-info"))
             assert isinstance(j, Mapping)
             return j.get("lock", False) is True
 
