e3a283e88dedc61d6d12207817c1498e54fb25f6
Buck Evan <buck.2019@gmail.com>
2024-01-26 14:51:27 -0600

    tf-lock: update for "bare" terragrunt slice compat

--- lib/tf_lock/lib/env.sh
+++ lib/tf_lock/lib/env.sh
@@ -7,3 +7,21 @@ HOSTNAME="$HOST"
 
 export TF_LOCK_ENONE=2
 export TF_LOCK_EHELD=3
+
+export TACOS_GHA_HOME="${TACOS_GHA_HOME:-$(cd "$HERE"/../..; pwd)}"
+export PYTHONPATH="$TACOS_GHA_HOME${PYTHONPATH:+:$PYTHONPATH}"
+export PATH="$TACOS_GHA_HOME/bin${PATH:+:$PATH}}"
+
+tf_working_dir() {
+  root_module="$1"
+  if [[ -e "$root_module/terragrunt.hcl" ]]; then
+    env --chdir "$root_module" terragrunt terragrunt-info |
+      jq -r .WorkingDir
+  else
+    echo "$root_module"
+  fi
+}
+
+if [[ "${DEBUG:-}" ]]; then
+  set -x
+fi
