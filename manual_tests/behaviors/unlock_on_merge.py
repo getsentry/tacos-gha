cf6b82ad9c2596364faf3a3e35b8d6dc263b6a08
Ellison Marks <ellison.marks@sentry.io>
2024-01-30 15:48:40 -0800

    Gets the unlock_on_merge test working.

--- manual_tests/behaviors/unlock_on_merge.py
+++ manual_tests/behaviors/unlock_on_merge.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
-import pytest
-
 from manual_tests.lib import tacos_demo
-from manual_tests.lib.xfail import XFailed
 
 TEST_NAME = __name__
 
 
-@pytest.mark.xfail(raises=XFailed)
 def test(pr: tacos_demo.PR) -> None:
-    assert pr.check("Terraform Lock").wait().success
+    assert pr.check("Terraform Plan").wait().success
 
     since = pr.approve()
     assert pr.is_approved()
 
     pr.merge()
 
-    try:
-        assert pr.check("Terraform Unlock").wait(since, timeout=6).success
-    except AssertionError:
-        raise XFailed("Unlock not yet implemented.")
+    assert pr.check("Terraform Unlock", "tacos_unlock").wait(since).success
