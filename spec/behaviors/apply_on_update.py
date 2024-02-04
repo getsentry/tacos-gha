76b43ce4383050d3d6c0fb1283a90b35b6b10990
Buck Evan <buck.2019@gmail.com>
2024-02-04 11:49:46 -0600

    put core functionality in lib/tacos scripts
    lib/ci/tacos_ now just handles log formatting

--- spec/behaviors/apply_on_update.py
+++ spec/behaviors/apply_on_update.py
@@ -10,6 +10,8 @@ TEST_NAME = __name__
 
 from pytest import fixture
 
+from lib.parse import Parse
+
 
 @fixture
 def slices_cleanup() -> Callable[[Slices], None]:
@@ -25,3 +27,46 @@ def test(pr: tacos_demo.PR) -> None:
     since = pr.add_label(":taco::apply")
     assert pr.check("Terraform Apply").wait(since).success
     assert pr.slices.plan_is_clean()
+
+    comments = pr.get_comments_for_job("apply", since)
+    assert set(comments) == pr.slices.slices
+
+    for slice, comment in sorted(comments.items()):
+        assert (
+            Parse(comment).before.first("\n")
+            == f"### TACOS Apply: {pr.slices.subpath}/{slice}"
+        )
+
+        summary = (
+            Parse(comment).after.first("<summary>").before.first("</summary>")
+        )
+        assert summary.startswith("Apply complete! Resources: ")
+        # ... X added, Y changed, Z ...
+        assert summary.endswith(" destroyed.")
+
+        assert "<summary>Commands: (success)</summary>" in comment
+
+        commands: Parse = (
+            Parse(comment).after.last("</summary>").before.first("</details>")
+        )
+
+        assert "$ tf-lock-acquire .\ntf-lock-acquire: success: .(" in commands
+        # the next bit is github-username@fake-pr-domain, which seems tricky
+
+        assert """\
+$ sudo-gcp terragrunt run-all init
+You are authenticated for the next hour as: tacos-gha-tf-apply@sac-dev-sa.iam.gserviceaccount.com
+""" in commands
+
+        assert "tf-lock-release" not in commands
+
+        for command in (
+            f"cd {pr.slices.subpath}/{slice}",
+            f"sudo-gcp tf-lock-acquire",
+            f"sudo-gcp terragrunt run-all init",
+            f"sudo-gcp terragrunt run-all refresh",
+            f"sudo-gcp terragrunt run-all apply --auto-approve",
+        ):
+            line = f"\n$ {command}\n"
+            assert line in commands
+            commands = commands.after.first(line)
