3c23f8d078590ada500d39b9d6223cce588fd562
Buck Evan <buck.2019@gmail.com>
2024-02-08 10:58:57 -0600

    fix(test): "behavior: message on lock failure"

--- spec/behaviors/message_on_lock_failure.py
+++ spec/behaviors/message_on_lock_failure.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env py.test
 from __future__ import annotations
 
+from lib.sh import sh
 from spec.lib import tacos_demo
 from spec.lib.gh import gh
+from spec.lib.slice import Slice
 from spec.lib.slice import Slices
 
 # TODO: improve the conflict message: "lock failed, on slice prod/slice-3-vm, due to user1, PR #334 "
@@ -10,6 +12,34 @@ CONFLICT_MESSAGE = """
 $ sudo-gcp tf-lock-acquire
 You are authenticated for the next hour as: tacos-gha-tf-state-admin@sac-dev-sa.iam.gserviceaccount.com
 tf-lock-acquire: failure: not """
+
+
+def assert_there_can_be_only_one(slice: Slice, *prs: tacos_demo.PR) -> None:
+    sh.banner(f"Slice: {slice}")
+
+    checks: dict[tacos_demo.PR, gh.CheckRun] = {
+        pr: pr.check(
+            "Terraform Plan", f"tacos_plan ({pr.slices.subpath / slice})"
+        ).wait()
+        for pr in prs
+    }
+    assert {check.conclusion for check in checks.values()} == {
+        "SUCCESS",
+        "FAILURE",
+    }
+
+    for pr, check in checks.items():
+        comments = pr.get_comments_for_job("plan")
+        if check.conclusion == "SUCCESS":
+            print("Winner:", pr.url)
+            for slice in pr.slices:
+                assert CONFLICT_MESSAGE not in comments[slice]
+        elif check.conclusion == "FAILURE":
+            print("Loser:", pr.url)
+            for slice in pr.slices:
+                assert CONFLICT_MESSAGE in comments[slice]
+        else:
+            raise AssertionError(check)
 
 
 def test(
@@ -23,25 +53,5 @@ def test(
             slices, test_name, demo, tacos_branch, branch=2
         ) as pr2,
     ):
-        checks: dict[tacos_demo.PR, gh.CheckRun] = {
-            pr1: pr1.check("Terraform Plan").wait(),
-            pr2: pr2.check("Terraform Plan").wait(),
-        }
-
-        for pr, check in checks.items():
-            comments = pr.get_comments_for_job("plan")
-            if check.conclusion == "SUCCESS":
-                print("Winner:", pr.url)
-                for slice in pr.slices:
-                    assert CONFLICT_MESSAGE not in comments[slice]
-            elif check.conclusion == "FAILURE":
-                print("Loser:", pr.url)
-                for slice in pr.slices:
-                    assert CONFLICT_MESSAGE in comments[slice]
-            else:
-                raise AssertionError(check)
-
-        assert {check.conclusion for check in checks.values()} == {
-            "SUCCESS",
-            "FAILURE",
-        }
+        for slice in slices:
+            assert_there_can_be_only_one(slice, pr1, pr2)
