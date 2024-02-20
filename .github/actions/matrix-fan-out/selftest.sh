50b8c141fc9cbf56f17f22bda0a1f614ce72068a
Buck Evan <buck.2019@gmail.com>
2024-02-20 13:58:58 -0600

    updating matrix action, for real-world use

--- .github/actions/matrix-fan-out/selftest.sh
+++ .github/actions/matrix-fan-out/selftest.sh
@@ -6,20 +6,37 @@
 set -euo pipefail
 exec >&2  # our only output is logging
 
+gha-set-output() {
+  var="$1"
+  val="$(cat)"
+  tee -a "$GITHUB_OUTPUT" <<< "$var=$val"
+}
+gha-set-artifact() {
+  artifact="$1"
+  gha-set-output "artifact.$artifact" <<< "$artifact"
+  cat > "$artifact"
+}
+
 key="$1"
 
 set -x
 
 : Processing... "$key"
-outdir="a/b/c"
-mkdir -p "$outdir/x/y/z"
 
-echo "$key" > "$outdir/key.json"
-echo 1 > "$outdir/x/y/matrix.json"
+gha-set-output key <<< "$key"
+echo "title=Hello, $key!" | tee -a "$GITHUB_OUTPUT"
+
+"$GITHUB_ACTION_PATH/"square.py "$key" |
+  gha-set-artifact square.txt
 
 square=$((key ** 2))
-echo "$square" | tee "$outdir/x/y/z/matrix.json"
-echo "$RANDOM" | tee "$outdir/x/random.txt" "$outdir/x/y/z/random.json"
+echo "$square" | gha-set-output 'matrix'
 
 outdir="a/b/c"
+gha-set-artifact "$outdir"
+
+mkdir -p "$outdir/x/y/z"
+echo 1 > "$outdir/x/y/matrix.1.json"
+echo "$RANDOM" | tee "$outdir/x/random.txt" "$outdir/x/y/z/random.json"
+
 find "$outdir" -type f -print0 | xargs -0 head
