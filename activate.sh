a9d1cbe80b1b1f67ac0cc85964be6fa334d344f3
maxwelllwang <maxwell.wang@sentry.io>
2024-03-05 14:58:31 -0800

    fix: automatically install using make if flock isn't installed. and use bashsource to overcome shebang

--- activate.sh
+++ activate.sh
@@ -1,7 +1,14 @@
 #!/sourceme/bash
-_here="$(readlink -f "$(dirname "$0")")"
+_here="$(readlink -f "$(dirname "${BASH_SOURCE:-$0}")")"
 
 export TACOS_GHA_HOME="$_here"
+
+if ! command -v flock &> /dev/null; then
+    echo "flock is not installed. Running make..."
+    make -C "$TACOS_GHA_HOME" .done/brew
+fi
+
 export PATH="$TACOS_GHA_HOME/bin${PATH:+:$PATH}}"
+
 
 unset _here
