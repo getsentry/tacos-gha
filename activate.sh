d9fd4db4648e4dd62a586552ec728fc4828e39a9
maxwelllwang <maxwell.wang@sentry.io>
2024-03-07 11:12:59 -0800

    fix: update message

--- activate.sh
+++ activate.sh
@@ -3,7 +3,10 @@ _here="$(readlink -f "$(dirname "${BASH_SOURCE:-$0}")")"
 
 export TACOS_GHA_HOME="$_here"
 
+if ! command -v flock &> /dev/null; then
     echo "Installing missing dependencies... (flock)"
+    make -C "$TACOS_GHA_HOME" .done/brew
+fi
 
 export PATH="$TACOS_GHA_HOME/bin${PATH:+:$PATH}}"
 
