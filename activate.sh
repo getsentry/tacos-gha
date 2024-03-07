6a017ab002c6989ed98a3d262e8c0c1a5d610442
maxwelllwang <maxwelllwang@gmail.com>
2024-03-07 11:10:44 -0800

    Update activate.sh
    Co-authored-by: Buck Evan <workitharder@gmail.com>

--- activate.sh
+++ activate.sh
@@ -3,10 +3,7 @@ _here="$(readlink -f "$(dirname "${BASH_SOURCE:-$0}")")"
 
 export TACOS_GHA_HOME="$_here"
 
-if ! command -v flock &> /dev/null; then
-    echo "flock is not installed. Running make..."
-    make -C "$TACOS_GHA_HOME" .done/brew
-fi
+    echo "Installing missing dependencies... (flock)"
 
 export PATH="$TACOS_GHA_HOME/bin${PATH:+:$PATH}}"
 
