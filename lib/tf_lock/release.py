337b475880c2c62363d422b6545355b6bf3551cf
kneeyo1 <neo.huang@sentry.io>
2024-04-05 09:42:26 -0700

    write out debug msg for UserError

--- lib/tf_lock/release.py
+++ lib/tf_lock/release.py
@@ -121,11 +121,11 @@ def get_lock_info(root_module: Path) -> Tuple[bool, dict[str, str]]:
     return lock, assert_dict_of_strings(result)
 
 
-def tf_lock_release(root_module: Path, env: Environ) -> bool:
+def tf_lock_release(root_module: Path, env: Environ) -> UserError | None:
     lock, lock_info = get_lock_info(root_module)
     if not lock:
         info(f"tf-lock-release: success: {root_module}")
-        return True
+        return None
 
     tf_user = f"{get_current_user(env)}@{get_current_host(env)}"
     lock_user = lock_info["Who"]
@@ -141,15 +141,15 @@ def tf_lock_release(root_module: Path, env: Environ) -> bool:
                         lock_info["ID"],
                     )
                 )
-        except sh.CalledProcessError:
+        except sh.CalledProcessError as error:
             # error message was already printed by subcommand
-            return False
+            return UserError(code=error.returncode)
 
         info(f"tf-lock-release: success: {root_module}({lock_user})")
-        return True
+        return None
 
     else:
-        raise UserError(
+        return UserError(
             f"""\
 tf-lock-release: failure: not {lock_user}: {root_module}({tf_user})
 {TFLockUser.from_string(lock_user).eheld_message()}
@@ -187,10 +187,13 @@ def main() -> None:
     from os import environ
 
     with sh.quiet():
-        successes = 0
+        failures = 0
         for path in paths:
-            successes += tf_lock_release(path, env=environ.copy())
-        failures = len(paths) - successes
+            res = tf_lock_release(path, env=environ.copy())
+            if res is not None:
+                sh.debug(res)
+                failures += 1
+        successes = len(paths) - failures
         sh.info(f"Successfully unlocked {successes} slices.")
         sh.info(f"Failed to unlock {failures} slices.")
 
