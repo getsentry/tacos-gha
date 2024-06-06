cd90ecf09f7664f1f1bfe7970d971281b181e31e
Buck Evan <buck.evan@sentry.io>
2024-06-06 13:02:51 -0500

    wip: fix assertions

--- .github/actions/matrix-fan-in/selftest.sh
+++ .github/actions/matrix-fan-in/selftest.sh
@@ -4,10 +4,11 @@
 set -euo pipefail
 exec >&2  # our only output is logging
 HERE="$(dirname "$(readlink -f "$0")")"
+fan_in="**/$MY_MATRIX_IO_DIR"
 
 set -x
 sum=$(
-  find a/b/c -type f -name 'matrix*json' -print0 |
+  find "$fan_in" -type f -name 'matrix*json' -print0 |
     xargs -0 jq -s add
 )
 
@@ -19,8 +20,8 @@ if (( sum != expected )); then
   exit 123  # convention borrowed from xargs -- a test failed
 fi
 
-find a/b/c -type f -name 'random.txt' -print0 | xargs -0 head
-find a/b/c -type f -name 'random.json' -print0 | xargs -0 jq .
-find a/b/c -type f -name 'key.json' -print0 | xargs -0 cat | sort -n
+find "$fan_in" -type f -name 'random.txt' -print0 | xargs -0 head
+find "$fan_in" -type f -name 'random.json' -print0 | xargs -0 jq .
+find "$fan_in" -type f -name 'key.json' -print0 | xargs -0 cat | sort -n
 
-"$HERE/"selftest-summary.sh a/b/c
+"$HERE/"selftest-summary.sh "$fan_in"
