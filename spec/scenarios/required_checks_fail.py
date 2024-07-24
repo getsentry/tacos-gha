75e7d756bb5e7df34adc6e11c2604fdf3fece482
Lyn Nagara <lynnagara@users.noreply.github.com>
2024-07-24 14:44:40 -0700

    taco apply needs required checks
    tacos cannot be applied if required checks are failing

---
+++ spec/scenarios/required_checks_fail.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env py.test
+from __future__ import annotations
+
+from pathlib import Path
+
+from lib.sh import sh
+from spec.lib import tacos_demo
+from spec.lib.gh import gh
+from spec.lib.slice import Slices
+
+
+def test(
+    slices: Slices, test_name: str, demo: gh.LocalRepo, tacos_branch: str
+) -> None:
+    Path("required_check.fail").touch()
+    with tacos_demo.PR.opened_for_slices(
+        slices, test_name, demo, tacos_branch
+    ) as pr:
+        sh.banner("look at your plan")
+        plan = pr.get_plans()
+        assert plan
+
+        sh.banner("apply")
+        pr.approve()
+        assert pr.is_approved()
+
+        assert not pr.slices.plan_is_clean()
+        since = pr.add_label(":taco::apply")
+
+        assert pr.check("Terraform Apply").wait(since).failure
+        assert "PR has not passed all required checks" in pr.comments(since)
